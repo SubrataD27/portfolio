9c057fc893c17af9ed90b97d053c978129221e85
Subrata Dhibar <140489803+SubrataD27@users.noreply.github.com>
2024-01-14 23:10:44 +0530

    Add files via upload

---
+++ Portfolio website/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer","Competitive Programmer","Backend Developer","Cloud Enthusiast"],
+    typeSpeed:200,
+    backSpeed:200,
+    backDelay:500, 
+    loop:true
+});
